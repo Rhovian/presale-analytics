bc0baca1f680fc315c0e2c516155e7868d8bf629
k <mja>
2022-11-21 14:21:43 -0800

    add polygon & bsc, clean up

--- presales/queries.py
+++ presales/queries.py
@@ -5,6 +5,12 @@ import requests
 load_dotenv(find_dotenv())
 
 ETHERSCAN_KEY = os.environ.get("ETHERSCAN_KEY")
+POLYGON_KEY = os.environ.get("POLYGON_KEY")
+BSC_KEY = os.environ.get("BSC_KEY")
+
+ETH_URL_BASE = "https://api.etherscan.io/api?module=account"
+POLYGON_URL_BASE = "https://api.polygonscan.com/api?module=account"
+BSC_URL_BASE = "https://api.bscscan.com/api?module=account"
 
 
 class QueryHandler:
@@ -21,25 +27,49 @@ class QueryHandler:
             self.build_query_token()
 
         res = requests.get(self.query)
-        if res.status_code == 200:
-            return res.json()
+        res = res.json()
+
+        if res["status"] == "1":
+            return res
+        else:
+            raise Exception("Error in Query Module. Message: {}".format(res["result"]))
 
     def build_query_native(self):
-        if self.chain == "ethereum":
-            self.query = (
-                "https://api.etherscan.io/api?module=account&action=txlist&address="
-                + self.presale
-                + "&startblock=0&endblock=99999999&page=1&offset=10000&sort=asc&apikey="
-                + ETHERSCAN_KEY
-            )
+        base = ETH_URL_BASE
+        key = ETHERSCAN_KEY
+
+        if self.chain == "polygon":
+            base = POLYGON_URL_BASE
+            key = POLYGON_KEY
+        elif self.chain == "bsc":
+            base = BSC_URL_BASE
+            key = BSC_KEY
+
+        self.query = (
+            base
+            + "&action=txlist&address="
+            + self.presale
+            + "&startblock=0&endblock=99999999&page=1&offset=10000&sort=asc&apikey="
+            + key
+        )
 
     def build_query_token(self):
-        if self.chain == "ethereum":
-            self.query = (
-                "https://api.etherscan.io/api?module=account&action=tokentx&contractAddress="
-                + self.token
-                + "&address="
-                + self.presale
-                + "&page=1&offset=100&startblock=0&endblock=27025780&sort=asc&apikey="
-                + ETHERSCAN_KEY
-            )
+        base = ETH_URL_BASE
+        key = ETHERSCAN_KEY
+
+        if self.chain == "polygon":
+            base = POLYGON_URL_BASE
+            key = POLYGON_KEY
+        elif self.chain == "bsc":
+            base = BSC_URL_BASE
+            key = BSC_KEY
+
+        self.query = (
+            base
+            + "&action=tokentx&contractAddress="
+            + self.token
+            + "&address="
+            + self.presale
+            + "&page=1&offset=100&startblock=0&endblock=27025780&sort=asc&apikey="
+            + key
+        )
